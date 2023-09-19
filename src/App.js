b4a53c6658261d078b7b339c3c62dac5517be923
YONGHYUN JANG <jyh1203@naver.com>
2023-09-19 23:35:01 +0900

    project init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+import MainPageComponent from "./main";
+
+function App() {
+  return <MainPageComponent />;
+}
+
+export default App;
